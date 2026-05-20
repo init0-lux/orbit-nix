eb0bf966ec8a54421c73ca38aeb5acdcdf09e8a5
init0-lux <init0.lux@protonmail.com>
2026-05-21 02:17:43 +0530

    chore: initialize project structure and inspect upstream;

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix-native packaging and distribution layer for Expo Orbit";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    {
+      overlays.default = import ./overlay.nix;
+      nixosModules.default = import ./modules/nixos.nix;
+      homeManagerModules.default = import ./modules/home-manager.nix;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        packages = {
+          orbit = pkgs.callPackage ./pkgs/by-name/or/orbit/package.nix { };
+          orbit-fhs = pkgs.callPackage ./pkgs/orbit-fhs.nix { inherit (pkgs) orbit; };
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
